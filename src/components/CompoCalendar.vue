6c5addf11ca97def7ea52391166053f2d29ce73d
YorlinPacha <yorlin1517@gmail.com>
2023-07-17 10:04:40 +0200

    Arreglo style scoped en compCalendar

--- src/components/CompoCalendar.vue
+++ src/components/CompoCalendar.vue
@@ -86,7 +86,7 @@ onMounted(() => {
 
 </script>
 
-<style scoped>
+<style>
 @import "../../node_modules/@syncfusion/ej2-base/styles/material.css";
 @import "../../node_modules/@syncfusion/ej2-buttons/styles/material.css";
 @import "../../node_modules/@syncfusion/ej2-vue-calendars/styles/material.css";
